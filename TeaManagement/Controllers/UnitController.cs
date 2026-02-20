2a63503e4dcf83e356b097984c41875c0fdcd056
Nischal061 <neupanenischal517@gmail.com>
2026-02-20 08:04:40 +0545

    feat: Unit and Category Report

--- TeaManagement/Controllers/UnitController.cs
+++ TeaManagement/Controllers/UnitController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NToastNotify;
+using TeaManagement.Dtos;
 using TeaManagement.Entities;
+using TeaManagement.Repository.Interface;
 using TeaManagement.ViewModels;
 
 namespace TeaManagement.Controllers;
@@ -9,18 +11,22 @@ public class UnitController : Controller
 {
     private readonly ApplicationDbContext _context;
     private readonly IToastNotification _toastNotification;
+    private readonly IReportRepository _reportRepository;
 
-    public UnitController(ApplicationDbContext context, IToastNotification toastNotification)
+    public UnitController(ApplicationDbContext context, IToastNotification toastNotification,
+        IReportRepository reportRepository)
     {
         _context = context;
         _toastNotification = toastNotification;
+        _reportRepository = reportRepository;
     }
 
     public IActionResult Index()
     {
         return View();
     }
-[HttpPost]
+
+    [HttpPost]
     public async Task<IActionResult> AddUnit(UnitVm vm)
     {
         try
@@ -49,5 +55,11 @@ public class UnitController : Controller
             _toastNotification.AddErrorToastMessage(e.Message);
             return RedirectToAction("Index");
         }
+    }
+
+    public async Task<IActionResult> UnitReport()
+    {
+        var report = await _reportRepository.GetUnitReportAsync();
+        return View(report);
     }
 }
